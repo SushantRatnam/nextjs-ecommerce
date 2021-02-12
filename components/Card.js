aaf237376cc23b066e64216b355d0a552cd2c07a
Sushant Ratnam <sushant.ratnam@bhnetwork.com>
2021-02-12 22:20:26 +0530

    added transition on cards

--- components/Card.js
+++ components/Card.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function Card({ image, title, brandName, minAmount, maxAmount }) {
 	const { src: imageSrc, imageTitleTagText: imageTitle } = image;
 	return (
-		<li className='m-2.5 border border-solid border-gray-cardBorder shadow-card bg-gray-cardBackground flex-4-col'>
+		<li className='transform hover:scale-105 duration-200 m-2.5 border border-solid border-gray-cardBorder shadow-card hover:shadow-cardHover bg-gray-cardBackground flex-4-col'>
 			<Link href='#'>
 				<a title={imageTitle} className='grid-item image-card image-shadow'>
 					<div className='p-2'>
-						<Image className='p-2' alt={title} src={imageSrc} width='300' height='200' />
+						<Image className='p-2' alt={title} src={imageSrc} layout='responsive' width='3' height='2' />
 					</div>
 					<div className='px-2 py-3 border border-t border-solid border-gray-cardDescriptionBorderTop'>
 						<div className='flex justify-between'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
 			xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
 			'2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
 			card: '3px 3px 5px 0 rgb(0 0 0 / 22%)',
+			cardHover: '6px 6px 8px 0 rgb(0 0 0 / 22%)',
 			inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
 			none: 'none',
 		},
