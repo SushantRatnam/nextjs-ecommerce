07ce69918f09cab5d72be809167aaa189a66d75d
Sushant Ratnam <sushant.ratnam@bhnetwork.com>
2021-02-15 10:31:40 +0530

    add meta desc and featured card and article

---
+++ components/FeaturedCards.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+function FeaturedCards() {
+	return (
+		<section className='container flex flex-wrap justify-between pt-16 mx-auto'>
+			<article className='flex-1 mr-5'>
+				<h1 className='mb-3 text-5xl'>Gift Cards</h1>
+				<p className='mb-5'>
+					<strong>Show someone you care by sending an eGift today. Easy delivery, within minutes.</strong>
+				</p>
+				<p className='mb-5'>While you may not be able to visit with your loved ones or thank those supporting your local community in person, we have a simple solution. eGifts are a great way to give thanks, give a gift, or to simply show someone you’re thinking of them. With hundreds of brands to choose from, there is an eGift Card for everyone!</p>
+				<Link href='#'>
+					<button className='bg-blue-customBtn text-white text-xl p-2.5 w-auto'>Send an eGift Card</button>
+				</Link>
+			</article>
+			<div className='flex-1 '>hi</div>
+		</section>
+	);
+}
+export default FeaturedCards;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import FeaturedCards from '../components/FeaturedCards';
 import FeaturedLinks from '../components/FeaturedLinks';
 import Products from '../components/Products';
 
@@ -8,10 +9,12 @@ export default function Home({ giftcards }) {
 			<Head>
 				<title>Mini Mall</title>
 				<link rel='icon' href='/favicon.ico' />
+				<meta name='description' content=' A Gift Card for every occasion. Over 200 gift cards from your favorite retailers. Find great deals and promotions for all of your gift card needs.'></meta>
 			</Head>
 
 			<main>
-				<FeaturedLinks/>
+				<FeaturedLinks />
+				<FeaturedCards />
 				<section id='cardList' className='container mx-auto'>
 					<Products giftcards={giftcards} />
 				</section>
