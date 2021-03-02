4768b14e2f202f4ca940b8609afe896a8d608e30
Sushant Ratnam <sushant.ratnam@bhnetwork.com>
2021-03-02 14:44:00 +0530

    changed width and height to string

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ function Header() {
 			<li>
 				<a className='flex flex-col mr-6 text-xs text-center max-w-header'href=''>
 					<div className='mx-auto'>
-						<Image width={25} height={25} src='https://productimages.nimbledeals.com/nimblebuy/f4c49679771d1cb0178eeac16141c082' alt=''></Image>
+						<Image width='25' height='25' src='https://productimages.nimbledeals.com/nimblebuy/f4c49679771d1cb0178eeac16141c082' alt=''></Image>
 					</div>
 					<span>Home</span>
 				</a>
@@ -15,7 +15,7 @@ function Header() {
 			<li>
 				<a className='flex flex-col mr-6 text-xs text-center max-w-header'href=''>
 					<div className='mx-auto'>
-						<Image className='mx-auto' width={25} height={25} src='https://productimages.nimbledeals.com/nimblebuy/79af4aa8be3179351fe114e9c7238e' alt=''></Image>
+						<Image className='mx-auto' width='25' height='25' src='https://productimages.nimbledeals.com/nimblebuy/79af4aa8be3179351fe114e9c7238e' alt=''></Image>
 					</div>
 					<span>Check Balance</span>
 				</a>
@@ -23,7 +23,7 @@ function Header() {
 			<li>
 				<a className='flex flex-col mr-6 text-xs text-center max-w-header'href=''>
 					<div className='mx-auto'>
-						<Image width={25} height={25} src='https://productimages.nimbledeals.com/nimblebuy/d49e53e6b1da262bbda897ef9a12d360' alt=''></Image>
+						<Image width='25' height='25' src='https://productimages.nimbledeals.com/nimblebuy/d49e53e6b1da262bbda897ef9a12d360' alt=''></Image>
 					</div>
 					<span>Activate Pickup Gift Cards</span>
 				</a>
@@ -31,7 +31,7 @@ function Header() {
 			<li>
 				<a className='flex flex-col mr-6 text-xs text-center max-w-header'href=''>
 					<div className='mx-auto'>
-						<Image width={25} height={25} src='https://productimages.nimbledeals.com/nimblebuy/f9c45cd930691c839ffaecbc55458cb' alt=''></Image>
+						<Image width='25' height='25' src='https://productimages.nimbledeals.com/nimblebuy/f9c45cd930691c839ffaecbc55458cb' alt=''></Image>
 					</div>
 					<span>Sign In/Sign Up</span>
 				</a>
@@ -40,7 +40,7 @@ function Header() {
 				<a className='relative flex flex-col mr-6 text-xs text-center max-w-header'href=''>
 					<span className='absolute font-bold text-blue-800 -top-2 -right-2' >0</span>
 					<div className='mx-auto'>
-						<Image width={25} height={25} src='https://productimages.nimbledeals.com/nimblebuy/f19d8252497d8a5a5ba176f58b07f' alt=''></Image>
+						<Image width='25' height='25' src='https://productimages.nimbledeals.com/nimblebuy/f19d8252497d8a5a5ba176f58b07f' alt=''></Image>
 					</div>
 					<span>Cart</span>
 				</a>
