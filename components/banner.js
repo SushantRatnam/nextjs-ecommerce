e1a37876c00f6f8a3cf4832018d567f142d46cdf
Sushant Ratnam <sushant.ratnam@bhnetwork.com>
2021-03-02 15:59:07 +0530

    changed carousel library

--- components/banner.js
+++ components/banner.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useQuery, QueryClient, QueryClientProvider, useQueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Image from 'next/image';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const queryClient = new QueryClient();
 
@@ -30,12 +30,46 @@ function BannerData() {
 		return <Image src='http://placehold.it/1440x405' alt='placeholder' className='mx-auto' layout='responsive' width='1440' height='405'></Image>;
 	}
 
+
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 1,
+			slidesToSlide: 1,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 1,
+			slidesToSlide: 1,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 1,
+			slidesToSlide: 1,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+			slidesToSlide: 1,
+		},
+	};
+
 	return (
-		<Carousel autoPlay infiniteLoop emulateTouch showThumbs={false}>
+		<Carousel
+			swipeable={false}
+			draggable={false}
+			showDots={true}
+			responsive={responsive}
+			ssr={true} // means to render carousel on server-side.
+			infinite={true}
+			autoPlay={true}
+			autoPlaySpeed={3000}
+			keyBoardControl={true}>
 			{data.banner.map((item, idx) => {
 				return (
 					<div key={item.src}>
-						<Image className='mx-auto' alt={item.alt} src={item.src} layout='responsive' width='1440' height='405'></Image>
+						<Image className='mx-auto' alt={item.alt} src={item.src} layout='responsive' width={1440} height={405}></Image>
 					</div>
 				);
 			})}
